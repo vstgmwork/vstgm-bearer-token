03249b8d7e5f0a8abb668681a5a05d7c5440d8ad
stgmviknesh <stgmviknesh@catchpoint.com>
2026-03-13 01:57:54 +0530

    updating /heavy to heavy.html

--- server.js
+++ server.js
@@ -456,7 +456,7 @@ app.get("/response/:code", (req, res) => {
 app.get('/assetloader', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'asset_loader.html')));
 app.get('/spaasset', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'spa_asset_loader.html')));
 app.get('/csp-child', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'csp-child-block.html')));
-app.get('/heavy', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'marketplace_heavy.html')));
+// app.get('/heavy', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'marketplace_heavy.html')));
 
 app.get('/cdn-dns-failure', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'cdn-dns-failure.html')));
 
