9db7cd843e05c6a5ed56fe49805f5e60424a8ae6
Viknesh STGM <viknesh.stgm@logicmonitor.com>
2026-05-18 19:23:44 +0530

    modified index.html with hover

--- scripts/catchpoint-maintenance-count.js
+++ scripts/catchpoint-maintenance-count.js
@@ -1,8 +1,11 @@
 setStepName("VSTGM Maintenance");
-open("http://localhost:3010/api/v2/scheduled-maintenances/active.json");
-
+open("https://www.vstgm.co.in/api/v2/scheduled-maintenances/active.json");
 var extract2 = Catchpoint.extract("resp-content", "(.*)");
 var json_content2 = JSON.parse(extract2);
 var maintenance = (json_content2.scheduled_maintenances || []).length;
+setIndicator("main1", maintenance);
+setIndicator("main2", maintenance);
 
+setStepName("Opening Google");
 open("https://www.google.com/search?q=" + maintenance);
+pause(5000);
